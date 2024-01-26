4e79e320cdb266616582cce9fe01f2cc19f7899e
robidful <150020689+robidful@users.noreply.github.com>
2024-01-26 17:46:02 +0000

    replaced icon url with a dynamic one

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
+import Weatherinfo from "./Weatherinfo";
 import "./index.css";
 import axios from "axios";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -13,17 +14,36 @@ export default function Weather(props) {
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
     });
+  }
+
+  function search() {
+    const apiKey = "d5523e724fae55536f5f65b8b74358f2";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
   }
 
   if (weatherData.ready) {
     return (
       <div className="weather-app">
         <header>
-          <form className="search-form" id="search-form">
+          <form
+            className="search-form"
+            id="search-form"
+            onSubmit={handleSubmit}
+          >
             <input
               type="search"
               placeholder="Enter a city.."
@@ -31,82 +51,16 @@ export default function Weather(props) {
               className="search-form-input"
               id="search-form-input"
               autoFocus="on"
+              onChange={handleCityChange}
             />
             <input type="submit" value="Search" class="search-form-button" />
           </form>
         </header>
-        <main>
-          <div className="row">
-            <div className="col-6">
-              <h1 className="weather-app-city" id="city">
-                {weatherData.city}
-              </h1>
-              <p className="weather-app-details">
-                <span id="time">
-                  <FormattedDate date={weatherData.date} />
-                </span>
-                ,{" "}
-                <span id="description" className="text-capitalize">
-                  {weatherData.description}
-                </span>
-                <br />
-                Humidity: <strong id="humidity">{weatherData.humidity}%</strong>
-                , Wind:
-                <strong id="wind-speed"> {weatherData.wind} km/h</strong>
-              </p>
-            </div>
-
-            <div className="col-6">
-              <img
-                src={weatherData.iconUrl}
-                alt="Mostly cloudy"
-                className="icon"
-              />
-              <span className="weather-app-temperature">
-                {Math.round(weatherData.temperature)}
-              </span>{" "}
-              <span className="unit">°C</span>
-            </div>
-          </div>
-          <div className="weather-forecast" id="forecast"></div>
-        </main>
-
-        <footer>
-          This project was coded by
-          <a
-            href="https://github.com/robidful"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            Roberta Di Cio
-          </a>
-          , is
-          <a
-            href="https://github.com/robidful/my-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            open-sourced on GitHub
-          </a>{" "}
-          and
-          <a
-            href="https://sprightly-dragon-57cd0f.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            hosted on Netlify
-          </a>
-        </footer>
+        <Weatherinfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "d5523e724fae55536f5f65b8b74358f2";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 }
---
+++ src/Weatherinfo.js
@@ -0,0 +1,70 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function Weatherinfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <main>
+        <div className="row">
+          <div className="col-6">
+            <h1 className="weather-app-city" id="city">
+              {props.data.city}
+            </h1>
+            <p className="weather-app-details">
+              <span id="time">
+                <FormattedDate date={props.data.date} />
+              </span>
+              ,{" "}
+              <span id="description" className="text-capitalize">
+                {props.data.description}
+              </span>
+              <br />
+              Humidity: <strong id="humidity">{props.data.humidity}%</strong>,
+              Wind:
+              <strong id="wind-speed"> {props.data.wind} km/h</strong>
+            </p>
+          </div>
+
+          <div className="col-6">
+            <img
+              src={props.data.iconUrl}
+              alt="Mostly cloudy"
+              className="icon"
+            />
+            <span className="weather-app-temperature">
+              {Math.round(props.data.temperature)}
+            </span>{" "}
+            <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="weather-forecast" id="forecast"></div>
+      </main>
+
+      <footer>
+        This project was coded by
+        <a href="https://github.com/robidful" target="_blank" rel="noreferrer">
+          {" "}
+          Roberta Di Cio
+        </a>
+        , is
+        <a
+          href="https://github.com/robidful/my-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>{" "}
+        and
+        <a
+          href="https://sprightly-dragon-57cd0f.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
+}
